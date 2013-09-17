ff7aab0ceefb15bab638037b32eaa3dd7ef39f3c
William Huang <wwhuang@umich.edu>
2013-09-17 19:37:01 -0400

    removed logging of passwords

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module V3Rails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,5 +25,6 @@ module V3Rails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
