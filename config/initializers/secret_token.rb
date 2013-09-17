aa3c594a2836c42de971b621f2284b472c7fad40
William Huang <wwhuang@umich.edu>
2013-09-17 16:24:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+V3Rails::Application.config.secret_key_base = '135ae529f450cc245a0a14d246386ab04b4db1ab2118b6a92de0d8d7206636da2f95cfcb31358467270907c3e6ccc9256abc79e4dc154d0d68d399d3e2ade562'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+V3Rails::Application.config.session_store :cookie_store, key: '_v3-rails_session'
