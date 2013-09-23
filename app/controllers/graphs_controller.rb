8a2492aa1949fcbb65578e0362a162395b8ff24d
William Huang <wwhuang@umich.edu>
2013-09-23 00:06:53 -0400

    getting the graph generator to work

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,13 +1,20 @@
+require 'open-uri'
+require 'json'
+
 class GraphsController < ApplicationController
 	def graphs
 	end
 
 	def graph_generator
-		redirect_to :controller => 'graphs', :action => params[:graph_type], params
+		@repeat = params[:repeat].to_i
+		@data_url = params[:data_url]
+		@title = params[:title]
+		render params[:graph_type]
 	end
 
 	def update_data
-
+		data = JSON.parse(open(params[:update_url]).read)
+		render json: data
 	end
 
 	def chord
